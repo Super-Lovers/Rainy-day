c75f789e0d7f8440a065d9b36cd2962c74cfd750
Super-Lovers <n.v.ivanov@st.hanze.nl>
2020-08-22 20:37:39 +0300

    Cleaned up scenes and added transitions

--- Assets/Scripts/User Interface/InterfaceManager.cs
+++ Assets/Scripts/User Interface/InterfaceManager.cs
@@ -1,10 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InterfaceManager : MonoBehaviour
 {
+    [Header("The scene to load's name when any key is pressed")]
+    [SerializeField]
+    private string sceneToLoadOnPress;
+
     public void ToggleWindow(Object window) {
         GameObject obj = (GameObject)window;
         obj.SetActive(!obj.activeSelf);
         AudioManager.Instance.AudioController.PlaySound("Press");
+    }
+
+    private void Update() {
+        if (Input.anyKeyDown) {
+            SceneManager.LoadScene(sceneToLoadOnPress);
+        }
     }
 }
