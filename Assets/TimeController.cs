3c532f9588ddb49c7af434c32110108d99b5542c
Super-Lovers <n.v.ivanov@st.hanze.nl>
2020-09-05 17:41:40 +0300

    Made a clock and environment updates

---
+++ Assets/TimeController.cs
@@ -0,0 +1,71 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI clock_text;
+
+    [Header("Environmental elements")]
+    [SerializeField]
+    private GameObject background_elements;
+
+    private float timer = 0;
+
+    // Clock parameters
+    private int hour = 17;
+    private int minutes = 20;
+
+    private float minute_increment = 0.30f;
+    private float current_minute_increment = 0;
+
+    // Background elements references
+    private Color current_bg_color;
+    private float color_decrement = 0.001f;
+
+    private void Start() {
+        current_bg_color = new Color(1, 1, 1, 1);
+    }
+
+    private void Update() {
+        if (timer >= 1) {
+            if (current_minute_increment == 0.60f) {
+                minutes++;
+
+                if (minutes == 60) {
+                    hour++;
+                    minutes = 0;
+                }
+
+                string minutesStr = minutes < 10 ? "0" + minutes : minutes.ToString();
+                if (clock_text != null) {
+                    clock_text.text = string.Format("{0}:{1}", hour, minutesStr);
+                }
+
+                current_minute_increment = 0;
+
+                if (current_bg_color.r > 0.2f) {
+                    DarkenBackgroundElements();
+                }
+            }
+
+            current_minute_increment += minute_increment;
+
+            timer = 0;
+        }
+
+        timer += Time.deltaTime;
+    }
+
+    private void DarkenBackgroundElements() {
+        current_bg_color = new Color(
+                current_bg_color.r - color_decrement,
+                current_bg_color.g - color_decrement,
+                current_bg_color.b - color_decrement, 255);
+
+        for (int i = 0; i < background_elements.transform.childCount; i++) {
+            Renderer element_shader = background_elements.transform.GetChild(i).GetComponent<Renderer>();
+            element_shader.material.SetColor("_Color", current_bg_color);
+        }
+    }
+}
